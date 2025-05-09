72ed58fef8fc07331ef5a54dfb97ec762fef29e2
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-05-09 11:04:54 +0900

    ブロック順番を制限するためのcontinueの追加忘れを修正

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -39,11 +39,13 @@ void GameScene::Initialize() {
 	for (uint32_t i = 0; i < kNumBlockVirtical; i++) {
 		for (uint32_t j = 0; j < kNumBlockHorizontal; j++) {
 			if ((i + j) % 2 == 1) {
+				continue;
+			}
 				worldTransformBlocks_[i][j] = new WorldTransform();
 				worldTransformBlocks_[i][j]->Initialize();
 				worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
 				worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
-			}
+			
 		}
 	}
 
